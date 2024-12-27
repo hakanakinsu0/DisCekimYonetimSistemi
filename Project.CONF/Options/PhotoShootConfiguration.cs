95d32a3e20c2f4fc027b6e8c17300dc3234dc70f
hakanakinsu0 <hakanakinsu@outlook.com>
2024-12-28 02:34:49 +0300

    Ilgili katmanlarda revizeler yapildi.

--- Project.CONF/Options/PhotoShootConfiguration.cs
+++ Project.CONF/Options/PhotoShootConfiguration.cs
@@ -18,6 +18,38 @@ namespace Project.CONF.Options
             Property(x => x.ShootTime).HasColumnName("Çekim Saati");
             Property(x => x.PhotoShootStatus).HasColumnName("Çekim Durumu");
             Property(x => x.Notes).HasColumnName("Notlar");
+
+            // Cascade delete kapatma düzenlemeleri
+
+            // Customer ile ilişki (1 Müşteri -> N Çekim)
+            HasRequired(ps => ps.Customer)
+                .WithMany(c => c.PhotoShoots)
+                .HasForeignKey(ps => ps.CustomerId)
+                .WillCascadeOnDelete(false);
+
+            // Location ile ilişki (1 Mekan -> N Çekim)
+            HasRequired(ps => ps.Location)
+                .WithMany(l => l.PhotoShoots)
+                .HasForeignKey(ps => ps.LocationId)
+                .WillCascadeOnDelete(false);
+
+            // Photographer ile ilişki (1 Fotoğrafçı -> N Çekim)
+            HasRequired(ps => ps.Photographer)
+                .WithMany(p => p.PhotoShoots)
+                .HasForeignKey(ps => ps.PhotographerId)
+                .WillCascadeOnDelete(false);
+
+            // Payments ile ilişki (1 Çekim -> N Ödeme)
+            HasMany(ps => ps.Payments)
+                .WithRequired(p => p.PhotoShoot)
+                .HasForeignKey(p => p.PhotoShootId)
+                .WillCascadeOnDelete(false);
+
+            // Albums ile ilişki (1 Çekim -> N Albüm)
+            HasMany(ps => ps.Albums)
+                .WithRequired(a => a.PhotoShoot)
+                .HasForeignKey(a => a.PhotoShootId)
+                .WillCascadeOnDelete(false);
         }
     }
 }
--- Project.ENTITIES/Models/PhotoShoot.cs
+++ Project.ENTITIES/Models/PhotoShoot.cs
@@ -24,6 +24,6 @@ namespace Project.ENTITIES.Models
         public virtual Photographer Photographer { get; set; } // 1 photographer n photoshoot, 1 photoshoot 1 photographer
         public virtual Location Location { get; set; } //1 location n photoshoot, 1 photoshoot 1 location
         public virtual List<Payment> Payments { get; set; } //1 photoshoot n Payment, 1 payment 1 photoshoot
-        public virtual List<PhotoShoot> PhotoShoots { get; set; } // 1 photoshop n album, 1 album 1 photoshop
+        public virtual List<Album> Albums { get; set; } // 1 photoshop n album, 1 album 1 photoshop
     }
 }
